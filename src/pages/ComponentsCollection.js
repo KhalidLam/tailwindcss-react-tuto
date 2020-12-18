866b8f517ec2b5d440d0dedf5b01c9187ed6932e
KhalidLam <lamsadikhalid@gmail.com>
2020-12-18 11:04:14 +0100

    init commit

---
+++ src/pages/ComponentsCollection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Pagination from '../components/Pagination';
+
+export default function ComponentsCollection() {
+  return (
+    <div className=''>
+      <Pagination></Pagination>
+    </div>
+  );
+}
